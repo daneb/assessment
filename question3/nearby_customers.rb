9f05e7cabde45221be92c4869bdf87c1efe3b677
daneb <dane.balia@hetzner.co.za>
2015-09-30 05:17:11 +0200

    Added in haversine and results of those found in 100km

--- question3/nearby_customers.rb
+++ question3/nearby_customers.rb
@@ -1,3 +1,43 @@
-module NearbyCustomers
-  
+require './helpers/haversine'
+
+class NearbyCustomers
+  attr_accessor :customers
+  @@dublin_office_lat = 53.3381985
+  @@dublin_office_long = -6.2592576
+
+  def initialize(all_customers)
+    @customers = all_customers["customers"]["items"]
+  end
+
+  def get_scrubbed_customer_list
+    get_unique_customers
+    ignore_customers_with_invalid_names
+  end
+
+  def get_customers_within_radius
+    get_scrubbed_customer_list
+    get_list_of_customers_within_radius
+  end
+
+  private
+  def get_unique_customers
+    @customers.uniq!
+  end
+
+  def ignore_customers_with_invalid_names
+    @customers =  @customers.select { |customer| !!(customer["name"] =~ /[a-z]/i) }
+  end
+
+  def get_list_of_customers_within_radius
+    customers_in_radius = []
+    @customers.each do |customer|
+      distance_between = get_distance_to_dublin_office(customer["latitude"].to_f, customer["longitude"].to_f)
+      customers_in_radius << customer if distance_between < 100
+    end
+    customers_in_radius
+  end
+
+  def get_distance_to_dublin_office(lat1, long1)
+    Helpers::Haversine.distance_between_two_points(lat1, long1, @@dublin_office_lat, @@dublin_office_long)
+  end
 end
--- question3/providers/json_text_file.rb
+++ question3/providers/json_text_file.rb
@@ -21,6 +21,8 @@ module Adaptor
       json_validator = Helpers::JSONValidation.new(file_contents)
       return JSON.parse(file_contents) if json_validator.valid_json?
       JSON.parse(json_validator.reformat_to_remove_multiple_json_root)
+    rescue  JSON::ParserError => e
+      Helpers::StatusEnvelope.new(e.to_s, "Corrupt data source - unable to parse")
     end
 
     def read_file
